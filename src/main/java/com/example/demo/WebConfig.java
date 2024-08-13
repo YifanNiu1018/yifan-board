466f39f192fed485ad289fd307d06e82c1ad6d21
YifanNiu <yifanniu@smail.nju.edu.cn>
2024-08-13 16:32:56 +0800

    Add uploadFile function

--- src/main/java/com/example/demo/WebConfig.java
+++ src/main/java/com/example/demo/WebConfig.java
@@ -1,8 +1,10 @@
+
 package com.example.demo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,12 +13,20 @@ public class WebConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
+
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:5173")  // 允许前端的来源
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                         .allowedHeaders("*");
+            }
+
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                // 将 "/uploads/**" 映射到本地文件系统中的 "D:/Code/Web/File/"
+                registry.addResourceHandler("/uploads/**")
+                        .addResourceLocations("file:D:/Code/Web/File/");
             }
         };
     }
